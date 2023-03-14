b74003e01f007adad6c2ca3bf13bb32d295dff57
MichaelFergie <michaelferguson2020@gmail.com>
2023-03-14 15:03:17 -0400

    Testing the Commit

---
+++ src/main/java/edu/miamiohcrafters/shv2/items/ModItems.java
@@ -0,0 +1,9 @@
+package edu.miamiohcrafters.shv2.items;
+
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ModItems {
+
+    @GameRegistry.ObjectHolder("shv2:testitem")
+    public static TestItem testItem;
+}
