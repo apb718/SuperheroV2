588f106a1c3e15faf43a6cbdfc63f3143858447d
apb718 <apb718@gmail.com>
2023-03-14 21:44:45 -0400

    Added a named tab, still need to add items from the mod to the tab, and to adjust the icon.

--- src/main/java/net/mucrafters/superhero/SuperheroMod.java
+++ src/main/java/net/mucrafters/superhero/SuperheroMod.java
@@ -1,6 +1,8 @@
 package net.mucrafters.superhero;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -10,7 +12,9 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
+
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(SuperheroMod.MOD_ID)
@@ -26,6 +30,9 @@ public class SuperheroMod {
 
         MinecraftForge.EVENT_BUS.register(this);
     }
+
+
+
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
@@ -44,4 +51,13 @@ public class SuperheroMod {
 
         }
     }
+
+
+    public static final CreativeModeTab SUPERHERO_TAB = new CreativeModeTab("superheromod") {
+        @Override
+        public @NotNull ItemStack makeIcon() {
+            return new ItemStack(Blocks.CUT_COPPER_SLAB);
+        }
+    };
+
 }
